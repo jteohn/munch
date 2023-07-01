d54e830b8d30badc5d9001b5b6012bfb147b423f
Snowlyst <tclam.snow@gmail.com>
2023-07-01 22:24:46 +0800

    tested api, pushing up Bmi Calculator and CalorieRequirement components

---
+++ src/Components/ApiTest1.js
@@ -0,0 +1,46 @@
+import { React, useState } from "react";
+import axios from "axios";
+
+function ApiTest() {
+  const [input, setInput] = useState("");
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchString = input;
+    axios({
+      method: "get",
+      headers: { "X-Api-Key": process.env.REACT_APP_CALORIENINJA_KEY },
+      url: `https://api.calorieninjas.com/v1/nutrition?query=${searchString}`,
+      contentType: "application/json",
+    })
+      .then((data) => {
+        console.log(data);
+        setInput("");
+      })
+      .catch((error) => {
+        console.log(`error: ${error}`);
+      });
+  };
+
+  return (
+    <div>
+      <div>API TEST</div>
+      <div>
+        <div>CalorieNinja API TEST</div>
+        <input
+          type="text"
+          placeholder="search"
+          value={input}
+          onChange={(e) => handleChange(e)}
+        />
+        <input type="submit" value="submit" onClick={handleSubmit} />
+      </div>
+    </div>
+  );
+}
+
+export default ApiTest;
--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,6 +1,4 @@
 import React from "react";
-import BMICalculator from "../Components/BMICalculator";
-import CalorieRequirement from "../Components/CalorieRequirement";
 
 // all the private info. Lock it with a lock!
 
@@ -8,8 +6,6 @@ export default function Profile() {
   return (
     <div>
       <h1>Profile Page!</h1>
-      <BMICalculator />
-      <CalorieRequirement />
     </div>
   );
 }
