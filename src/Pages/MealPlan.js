d355b53731b821f4ea4c7ea9a530a4af35e0fd43
jteohn <jteohn@gmail.com>
2023-07-07 18:25:34 +0800

    added function to total up calories in mealplan.js

--- src/Pages/MealPlan.js
+++ src/Pages/MealPlan.js
@@ -9,6 +9,9 @@ export default function MealPlan() {
   const [totalResults, setTotalResults] = useState([]);
   const [displayData, setDisplayData] = useState(null);
 
+  const [foodName, setFoodName] = useState("");
+  const [addMeal, setAddMeal] = useState([]);
+
   const handleSearch = (e) => {
     e.preventDefault();
     axios({
@@ -18,18 +21,50 @@ export default function MealPlan() {
       contentType: "application/json",
     })
       .then((result) => {
-        setTotalResults((prevResults) => [...prevResults, result.data]);
+        // Added a condition to check if the ingredient exists in our API.
+        if (result.data.items.length === 0) {
+          console.log(
+            "`oops, you caught us! We are still working to include all the ingredients!`"
+          );
+        } else {
+          setTotalResults((prevResults) => [...prevResults, result.data]);
+        }
         setIngredientSearchQuery("");
       })
       .catch((error) => {
         console.log(`error: ${error}`);
       });
   };
+
+  const handleAddCalories = () => {
+    const addCalories = displayData.reduce(
+      (sum, result) => sum + result.calories,
+      0
+    );
+
+    const newMealPlan = {
+      nameOfFood: foodName,
+      totalCalories: addCalories.toFixed(2),
+    };
+
+    setAddMeal([...addMeal, newMealPlan]);
+
+    console.log(`add calories:`, addCalories);
+    console.log(`new meal plan:`, newMealPlan);
+  };
+
+  // open modal to allow user to input the name
+  // const handleInputChange = (e) => {
+  //   setFoodName(e.target.value);
+  // };
+
   const handleReset = (e) => {
     setTotalResults([]);
   };
 
+  // Displays results from totalResults array & update the displayData state with the transformed data.
   useEffect(() => {
+    // data stores the object (aka transformed data) from totalResults
     const data = totalResults.map((result) => {
       return {
         servingSize: result.items[0].serving_size_g,
@@ -37,33 +72,62 @@ export default function MealPlan() {
         calories: result.items[0].calories,
       };
     });
+    // data is then passed to displayData & update state
     setDisplayData(data);
   }, [totalResults]);
+  // dependency array - useEfect is called whenever totalResults changes.
+
   return (
     <div>
       <h1>Meal Plan</h1>
       <div>
-        <div>Start Planning your meal by searching up ingredients below!</div>
+        <div>Start planning your meal by searching up the ingredients!</div>
         <input
           type="text"
           placeholder="search"
           value={ingredientSearchQuery}
           onChange={(e) => setIngredientSearchQuery(e.target.value)}
         />
-        <input type="submit" value="submit" onClick={handleSearch} />
+        <button onClick={handleSearch}>Add Item</button>
       </div>
-      {displayData
+      {displayData && displayData.length > 0
         ? displayData.map((data, index) => (
             <div key={index + 1}>
-              <div>
-                {data.servingSize}g of {data.name}: {data.calories}kcal
-              </div>
+              <ul>
+                <li>
+                  {data.servingSize}g of {data.name}: {data.calories}kcal
+                </li>
+              </ul>
             </div>
           ))
         : null}
+
+      <br />
+      <input
+        type="text"
+        value={foodName}
+        placeholder="Let's give it a name!"
+        onChange={(e) => setFoodName(e.target.value)}
+      />
+      <button onClick={handleAddCalories}>Add Meal</button>
       <button onClick={handleReset}>Reset Foods</button>
       <br />
-      {/* <Calendar /> */}
+      <br />
+      <Calendar />
     </div>
   );
 }
+
+// if ingredient cannot be found (aka return undefined) alert user to key in another ingredient or similar ingredient
+
+// {displayData
+//   ? displayData.map((data, index) => (
+//       <div key={index + 1}>
+//         <ul>
+//           <li>
+//             {data.servingSize}g of {data.name}: {data.calories}kcal
+//           </li>
+//         </ul>
+//       </div>
+//     ))
+//   : null}
--- src/Pages/Recipe.js
+++ src/Pages/Recipe.js
@@ -1,115 +1,112 @@
-import React, { useState } from "react";
-import axios from "axios";
-import { Card } from "react-bootstrap";
-import { Modal } from "react-responsive-modal";
-import { Button } from "react-bootstrap";
-import "react-responsive-modal/styles.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import React, { useState } from "react";
+// import axios from "axios";
+// import { Card } from "react-bootstrap";
+// import { Modal } from "react-responsive-modal";
+// import { Button } from "react-bootstrap";
+// import "react-responsive-modal/styles.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 export default function Recipe() {
-  const [recipeSearchQuery, setRecipeSearchQuery] = useState("");
-  const [mealTypeQuery, setMealTypeQuery] = useState("Breakfast");
-  const [modalStatus, setModalStatus] = useState(false);
-  const [recipeResults, setRecipeResults] = useState(null);
-  const [selectedRecipeIndex, setSelectedRecipeIndex] = useState(null);
-
-  const openModal = (index) => {
-    console.log("setModalStatus set to true");
-    setSelectedRecipeIndex(index);
-    setModalStatus(true);
-  };
-  const closeModal = () => setModalStatus(false);
-
-  const handleSearch = (e) => {
-    e.preventDefault();
-    console.log(`mealTypeQuery: ${mealTypeQuery}`);
-    console.log(`recipeSearchQuery: ${recipeSearchQuery}`);
-    axios
-      .get(
-        `https://api.edamam.com/api/recipes/v2?type=public&q=${recipeSearchQuery}&app_id=${process.env.REACT_APP_EDAMAM_RECIPE_ID}&app_key=${process.env.REACT_APP_EDAMAM_RECIPE_KEY}&diet=balanced&mealType=${mealTypeQuery}&dishType=Main%20course&dishType=Salad&dishType=Soup&dishType=Starter&imageSize=REGULAR&field=uri&field=image&field=images&field=source&field=url&field=ingredientLines&field=calories&field=totalWeight&field=totalTime&field=mealType&field=totalNutrients&field=totalDaily`
-      )
-      .then((result) => {
-        setRecipeSearchQuery("");
-        console.log(result.data.hits);
-        const savedRecipes = result.data.hits;
-        setRecipeResults(savedRecipes);
-      })
-      .catch((error) => {
-        console.log(`error: ${error}`);
-      });
-  };
-  const changeMealType = (e) => {
-    setMealTypeQuery(e.target.value);
-  };
-  return (
-    <div>
-      <h1>Recipes!</h1>
-
-      <div>
-        <form>
-          <label>Select Meal Time</label>
-        </form>
-        <select value={mealTypeQuery} onChange={changeMealType}>
-          <option value={"Breakfast"}>Breakfast</option>
-          <option value={"Lunch"}>Lunch</option>
-          <option value={"Dinner"}>Dinner</option>
-        </select>
-      </div>
-      <br />
-      <div>
-        <input
-          type="text"
-          placeholder="search"
-          value={recipeSearchQuery}
-          onChange={(e) => setRecipeSearchQuery(e.target.value)}
-        />
-        <input type="submit" value="submit" onClick={handleSearch} />
-      </div>
-      <div>
-        {recipeResults === null
-          ? "No Search Active!"
-          : recipeResults.map((result, indexed) => {
-              return (
-                <div key={indexed}>
-                  <Card>
-                    <Card.Img
-                      variant="top"
-                      src={result.recipe.images.THUMBNAIL.url}
-                      alt=""
-                      style={{ height: "100px", width: "100px" }}
-                    />
-                    <Card.Title>
-                      <a href={result.recipe.url}>Recipe {indexed + 1}</a>
-                    </Card.Title>
-                    <Card.Text>
-                      <div>Calories: {result.recipe.calories.toFixed(0)}</div>
-                      <Button
-                        variant="primary"
-                        onClick={() => openModal(indexed)}
-                      >
-                        Ingredient List
-                      </Button>
-                      {modalStatus &&
-                        recipeResults &&
-                        recipeResults[selectedRecipeIndex] && (
-                          <Modal open={modalStatus} onClose={closeModal} center>
-                            <h2>Ingredients:</h2>
-                            <ul>
-                              {recipeResults[
-                                selectedRecipeIndex
-                              ].recipe.ingredientLines.map(
-                                (ingredient, index) => (
-                                  <li key={index + 1}>{ingredient}</li>
-                                )
-                              )}
-                            </ul>
-                          </Modal>
-                        )}
-                    </Card.Text>
-                  </Card>
-                </div>
-              );
-            })}
-      </div>
-    </div>
-  );
+  //   const [recipeSearchQuery, setRecipeSearchQuery] = useState("");
+  //   const [mealTypeQuery, setMealTypeQuery] = useState("Breakfast");
+  //   const [modalStatus, setModalStatus] = useState(false);
+  //   const [recipeResults, setRecipeResults] = useState(null);
+  //   const [selectedRecipeIndex, setSelectedRecipeIndex] = useState(null);
+  //   const openModal = (index) => {
+  //     console.log("setModalStatus set to true");
+  //     setSelectedRecipeIndex(index);
+  //     setModalStatus(true);
+  //   };
+  //   const closeModal = () => setModalStatus(false);
+  //   const handleSearch = (e) => {
+  //     e.preventDefault();
+  //     console.log(`mealTypeQuery: ${mealTypeQuery}`);
+  //     console.log(`recipeSearchQuery: ${recipeSearchQuery}`);
+  //     axios
+  //       .get(
+  //         `https://api.edamam.com/api/recipes/v2?type=public&q=${recipeSearchQuery}&app_id=${process.env.REACT_APP_EDAMAM_RECIPE_ID}&app_key=${process.env.REACT_APP_EDAMAM_RECIPE_KEY}&diet=balanced&mealType=${mealTypeQuery}&dishType=Main%20course&dishType=Salad&dishType=Soup&dishType=Starter&imageSize=REGULAR&field=uri&field=image&field=images&field=source&field=url&field=ingredientLines&field=calories&field=totalWeight&field=totalTime&field=mealType&field=totalNutrients&field=totalDaily`
+  //       )
+  //       .then((result) => {
+  //         setRecipeSearchQuery("");
+  //         console.log(result.data.hits);
+  //         const savedRecipes = result.data.hits;
+  //         setRecipeResults(savedRecipes);
+  //       })
+  //       .catch((error) => {
+  //         console.log(`error: ${error}`);
+  //       });
+  //   };
+  //   const changeMealType = (e) => {
+  //     setMealTypeQuery(e.target.value);
+  //   };
+  //   return (
+  //     <div>
+  //       <h1>Recipes!</h1>
+  //       <div>
+  //         <form>
+  //           <label>Select Meal Time</label>
+  //         </form>
+  //         <select value={mealTypeQuery} onChange={changeMealType}>
+  //           <option value={"Breakfast"}>Breakfast</option>
+  //           <option value={"Lunch"}>Lunch</option>
+  //           <option value={"Dinner"}>Dinner</option>
+  //         </select>
+  //       </div>
+  //       <br />
+  //       <div>
+  //         <input
+  //           type="text"
+  //           placeholder="search"
+  //           value={recipeSearchQuery}
+  //           onChange={(e) => setRecipeSearchQuery(e.target.value)}
+  //         />
+  //         <input type="submit" value="submit" onClick={handleSearch} />
+  //       </div>
+  //       <div>
+  //         {recipeResults === null
+  //           ? "No Search Active!"
+  //           : recipeResults.map((result, indexed) => {
+  //               return (
+  //                 <div key={indexed}>
+  //                   <Card>
+  //                     <Card.Img
+  //                       variant="top"
+  //                       src={result.recipe.images.THUMBNAIL.url}
+  //                       alt=""
+  //                       style={{ height: "100px", width: "100px" }}
+  //                     />
+  //                     <Card.Title>
+  //                       <a href={result.recipe.url}>Recipe {indexed + 1}</a>
+  //                     </Card.Title>
+  //                     <Card.Text>
+  //                       <div>Calories: {result.recipe.calories.toFixed(0)}</div>
+  //                       <Button
+  //                         variant="primary"
+  //                         onClick={() => openModal(indexed)}
+  //                       >
+  //                         Ingredient List
+  //                       </Button>
+  //                       {modalStatus &&
+  //                         recipeResults &&
+  //                         recipeResults[selectedRecipeIndex] && (
+  //                           <Modal open={modalStatus} onClose={closeModal} center>
+  //                             <h2>Ingredients:</h2>
+  //                             <ul>
+  //                               {recipeResults[
+  //                                 selectedRecipeIndex
+  //                               ].recipe.ingredientLines.map(
+  //                                 (ingredient, index) => (
+  //                                   <li key={index + 1}>{ingredient}</li>
+  //                                 )
+  //                               )}
+  //                             </ul>
+  //                           </Modal>
+  //                         )}
+  //                     </Card.Text>
+  //                   </Card>
+  //                 </div>
+  //               );
+  //             })}
+  //       </div>
+  //     </div>
+  //   );
 }
